70e5250c4d4b17964ae9c88b475de16abacdd751
Adam Greenthal <adamgreenthal@gmail.com>
2020-02-01 18:51:56 -0800

    added files

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+document.getElementById("Confirm").addEventListener("click", foo);
+
+function save() {
+    prompt(document.getElementById("questionOne").value);
+}
+
+<input type="button" id="confirm" value="Confirm">
+});
+
+
+{/* Was sick the day we went over Local storage. Going to spend the morning reviewing it on my own. */}
